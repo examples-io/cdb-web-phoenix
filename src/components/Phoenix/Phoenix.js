85fb5f6daa960f6ec9f275604f76f5e4e8444678
Bobby Nannier <bobby@nannier.com>
2018-02-03 16:36:37 -0500

    feat: Phoenix Componant added

---
+++ src/components/Phoenix/Phoenix.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { Provider } from 'react-redux'
+import { BrowserRouter as Router } from 'react-router-dom'
+
+import configureStore from '../../utilities/configureStore'
+import registerServiceWorker from '../../utilities/registerServiceWorker'
+
+function Phoenix(routes, reducers) {
+  const store = configureStore(reducers);
+
+  const routeComponents = (
+      <div>{routes}</div>
+  );
+
+  ReactDOM.render(
+      <Provider store={store}>
+        <div>
+          <Router>
+            {routeComponents}
+          </Router>
+        </div>
+      </Provider>
+      , document.getElementById('root')
+  );
+  registerServiceWorker();
+}
+
+export default Phoenix;
