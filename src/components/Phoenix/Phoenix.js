69d237c67bbf6ad628f092e704bed8a2c271a48f
Bobby Nannier <bobby@nannier.com>
2018-02-03 17:40:34 -0500

    feat: Phoenix added

--- src/components/Phoenix/Phoenix.js
+++ src/components/Phoenix/Phoenix.js
@@ -7,23 +7,23 @@ import configureStore from '../../utilities/configureStore'
 import registerServiceWorker from '../../utilities/registerServiceWorker'
 
 function Phoenix(routes, reducers) {
-  const store = configureStore(reducers);
+    const store = configureStore(reducers);
 
-  const routeComponents = (
-      <div>{routes}</div>
-  );
+    const routeComponents = (
+        <div>{routes}</div>
+    );
 
-  ReactDOM.render(
-      <Provider store={store}>
-        <div>
-          <Router>
-            {routeComponents}
-          </Router>
-        </div>
-      </Provider>
-      , document.getElementById('root')
-  );
-  registerServiceWorker();
+    ReactDOM.render(
+        <Provider store={store}>
+            <div>
+                <Router>
+                    {routeComponents}
+                </Router>
+            </div>
+        </Provider>
+        , document.getElementById('root')
+    );
+    registerServiceWorker();
 }
 
 export default Phoenix;
